9b480278a989bb68e8e54659d2b40fafd6cd39bf
Paulo Borges <victorborges296@hotmail.com>
2024-05-15 08:33:46 -0300

    function validaSegundoDigito added to the project

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,14 +1,15 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
 
-    validaNumeroRepetidos(cpf);
-    validaPrimeiroDigito(cpf);
-
-    console.log(validaNumeroRepetidos(cpf));
-    console.log(validaPrimeiroDigito(cpf));
+    if (validaNumeroRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log("Esse cpf não existe!");
+    } else {
+        console.log("Existe!");
+    }
 }
 
 function validaNumeroRepetidos(cpf) {
+    // implementar função para montar o array numerosRepetidos
     const numerosRepetidos = [
         '00000000000',
         '11111111111',
@@ -36,9 +37,27 @@ function validaPrimeiroDigito(cpf) {
 
     soma = (soma * 10) % 11;
 
-    if (soma == 10 || soma == 11) {
+    if (soma == 10 || soma == 1) {
         soma = 0;
     }
 
     return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 1) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 }
